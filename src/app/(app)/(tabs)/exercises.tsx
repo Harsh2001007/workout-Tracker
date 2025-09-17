398510581877f1544f363a38fde3c69cdf598d32
Harsh <hs5686584@gmail.com>
2025-09-18 00:52:25 +0530

    exercise screen UI component and exercise get logic part done

--- src/app/(app)/(tabs)/exercises.tsx
+++ src/app/(app)/(tabs)/exercises.tsx
@@ -4,12 +4,35 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  RefreshControl,
 } from "react-native";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
 
 const Exerceises = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+  const [refreshing, setRefresing] = useState(false);
+  const [exercises, setExercises] = useState([]);
+  const [filteredExercises, setFilteredExercises] = useState([]);
+
+  const fetchExercise = async () => {
+    try {
+      const exercises = await client.fetch(exercisesQuery);
+      setExercises(exercises);
+      setFilteredExercises(exercises);
+    } catch (err) {
+      console.log("error fetching exercises : ", err);
+    }
+  };
+
+  const onRefresh = async () => {
+    setRefresing(true);
+    await fetchExercise();
+    setRefresing(false);
+  };
   return (
     <SafeAreaView className="flex-1 bg-gray-50">
       {/* Header  */}
@@ -40,7 +63,44 @@ const Exerceises = () => {
           )}
         </View>
       </View>
-      <Text>Exerceiseses</Text>
+
+      {/* Exercise List  */}
+
+      <FlatList
+        data={[]}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 24 }}
+        renderItem={({ item }) => (
+          <ExerciseCard
+            item={item}
+            onPress={() => router.push(`/exercise-detail?id=${item.id}`)}
+          />
+        )}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={["#3B82F6"]}
+            tintColor="#3B82F6"
+            title="Pull to refresh exercises"
+            titleColor="#6B7280"
+          />
+        }
+        ListEmptyComponent={
+          <View className="bg-white rounded-2xl p-8 items-center">
+            <Ionicons name="fitness-outline" size={64} color="#9CA3AF" />
+            <Text className="text-xl font-semibold text-gray-900 mt-4">
+              {searchQuery ? "No exercise found" : "Loading exercises..."}
+            </Text>
+            <Text className="text-gray-600 text-center mt-2">
+              {searchQuery
+                ? "Try adjusting your search"
+                : "Your exercises will appear here"}
+            </Text>
+          </View>
+        }
+      />
     </SafeAreaView>
   );
 };
