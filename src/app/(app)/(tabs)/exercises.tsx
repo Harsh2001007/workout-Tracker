b37d901f57f30dd6ff0021745521dd60a1dfc580
Harsh <hs5686584@gmail.com>
2025-09-18 00:18:48 +0530

    exercise screen header part done

--- src/app/(app)/(tabs)/exercises.tsx
+++ src/app/(app)/(tabs)/exercises.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const Exerceises = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-gray-50">
+      {/* Header  */}
+
+      <View className="px-6 py-4 bg-white borber-b border-gray-200">
+        <Text className="text-2xl font-bold text-gray-900">
+          Exercise Library
+        </Text>
+        <Text className="text-gray-600 mt-1">
+          Discover and master new exercises
+        </Text>
+
+        {/* Searchbar  */}
+
+        <View className="flex-row items-center bg-gray-100 rounded-xl px-4 py-3 mt-4">
+          <Ionicons name="search" size={20} color="#6B7280" />
+          <TextInput
+            className="flex-1 ml-3 text-gray-800"
+            placeholder="search exercises"
+            placeholderTextColor="#9CA3AF"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          {searchQuery.length > 0 && (
+            <TouchableOpacity onPress={() => setSearchQuery("")}>
+              <Ionicons name="close-circle" size={20} color="#6B7280" />
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
       <Text>Exerceiseses</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
